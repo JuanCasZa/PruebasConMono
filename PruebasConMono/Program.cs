e32c04a475ba78ca5a0fb8be3785d26ed199193f
JuanCasZa <juanjose-castro2@hotmail.com>
2025-08-23 11:20:55 -0500

    Posibles clases proyecto

--- PruebasConMono/Program.cs
+++ PruebasConMono/Program.cs
@@ -2,6 +2,30 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mono, muy buñuelo pues !!!");
 
-public class Personas {
+public class Personas
+{
+    public String nombre;
+    public String cedula;
+    public int edad;
+    public decimal estatura;
+    public decimal peso;
 
+}
+
+public class Entrenadores : Personas
+{
+    public String titulo;
+    public int anhosExperiencia;
+}
+
+public class Clientes : Personas
+{
+    public Membresias membresia;
+}
+
+public class Membresias
+{
+    public DateTime inicioMembresia;
+    public DateTime finMembresia;
+    public decimal valor;
 }
