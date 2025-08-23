80cc8ff0220251756737c13b54be00bad70fc829
JuanCasZa <juanjose-castro2@hotmail.com>
2025-08-22 21:27:22 -0500

    Prueba cambios

--- PruebasConMono/Program.cs
+++ PruebasConMono/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Mono, muy buñuelo pues !!!");
 
 public class Personas {
 
--- PruebasConMono/obj/Debug/net8.0/PruebasConMono.AssemblyInfo.cs
+++ PruebasConMono/obj/Debug/net8.0/PruebasConMono.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf7dfce4470e24d785a054e265856a264851649")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe8bc7f048b129cfa57bf07da0214317c2908629")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
