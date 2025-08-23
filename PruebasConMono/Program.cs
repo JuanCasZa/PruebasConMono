bc47846603157184a6791e913c9c798a1ca5b2e8
santibbc <santibedoyacastro@gmail.com>
2025-08-23 12:49:11 -0500

    cambio calculadora

--- PruebasConMono/Program.cs
+++ PruebasConMono/Program.cs
@@ -2,7 +2,66 @@
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mono, muy buñuelo pues !!!");
 Console.WriteLine("buñuelo ud apá!!!");
-Console.WriteLine("buñuelo ud apá!!!");
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("Calculadora simple");
+    Console.Write("Ingrese el primer número: ");
+    double num1;
+    while (!double.TryParse(Console.ReadLine(), out num1))
+    {
+        Console.Write("Por favor, ingrese un número válido: ");
+    }
+
+    Console.Write("Ingrese el segundo número: ");
+    double num2;
+    while (!double.TryParse(Console.ReadLine(), out num2))
+    {
+        Console.Write("Por favor, ingrese un número válido: ");
+    }
+
+    Console.WriteLine("Seleccione la operación (+, -, *, /): ");
+    string operacion = Console.ReadLine();
+
+    double resultado = 0;
+    bool operacionValida = true;
+
+    switch (operacion)
+    {
+        case "+":
+            resultado = num1 + num2;
+            break;
+        case "-":
+            resultado = num1 - num2;
+            break;
+        case "*":
+            resultado = num1 * num2;
+            break;
+        case "/":
+            if (num2 != 0)
+                resultado = num1 / num2;
+            else
+            {
+                Console.WriteLine("No se puede dividir por cero.");
+                operacionValida = false;
+            }
+            break;
+        default:
+            Console.WriteLine("Operación no válida.");
+            operacionValida = false;
+            break;
+    }
+
+    if (operacionValida)
+        Console.WriteLine($"El resultado es: {resultado}");
+
+    Console.Write("¿Desea realizar otra operación? (si/no): ");
+    string respuesta = Console.ReadLine();
+    if (respuesta.ToLower() != "si")
+        continuar = false;
+        
+}
 
 
 public class Personas {
--- PruebasConMono/obj/Debug/net8.0/PruebasConMono.AssemblyInfo.cs
+++ PruebasConMono/obj/Debug/net8.0/PruebasConMono.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0c1dfbf9132130358706fc3de68195e8e3b48f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f28776b6a7beb208b99e1067f0dd23df45061283")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebasConMono")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
