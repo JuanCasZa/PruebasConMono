090be991fbcabbb623e4a15152afc26ad46e717f
JuanCasZa <juanjose-castro2@hotmail.com>
2025-08-23 17:45:52 -0500

    Avances hasta el momento

--- PruebasConMono/Principal.sql
+++ PruebasConMono/Principal.sql
@@ -5,21 +5,26 @@ create table Principal
 	id int primary key,
 	nombreProducto varchar(30),
 	precioProducto decimal,
-	informacionProducto varchar(60),
-	categoriaProducto varchar(15),
-	stock int,
 	fechaIngreso date,
-	fechaVencimiento date,
 	imagen image,
-	proveedor varchar(30)
 );
 
 select * from Principal where 1 = 0;
 
-insert into Principal (id , precioProducto, informacionProducto, categoriaProducto, stock, fechaIngreso, fechaVencimiento, proveedor) values 
-	(1, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito'),
-	(2, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito'),
-	(3, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito'),
-	(4, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito'),
-	(5, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito'),
-	(6, 2000, '', '', 10, '2025-01-25', '2025-12-30', 'Exito')
+insert into Principal (id , nombreProducto, precioProducto, fechaIngreso) values 
+	(1, 'a', 2000, '2025-01-25'),
+	(2, 'b', 3000, '2025-02-25'),
+	(3, 'c', 4000, '2025-03-25'),
+	(4, 'd', 5000, '2025-04-25'),
+	(5, 'e', 6000, '2025-05-25'),
+	(6, 'f', 7000, '2025-06-25')
+
+select * from Principal
+
+insert into Principal (id , nombreProducto, precioProducto, fechaIngreso) values 
+	(7, 'g', 8000, '2025-07-25'),
+	(8, 'h', 9000, '2025-08-25'),
+	(9, 'i', 10000, '2025-09-25'),
+	(10, 'j', 11000, '2025-10-25')
+
+drop table Principal;
