f28776b6a7beb208b99e1067f0dd23df45061283
santibbc <santibedoyacastro@gmail.com>
2025-08-23 12:36:22 -0500

    segundo initento commit

--- PruebasConMono/Program.cs
+++ PruebasConMono/Program.cs
@@ -1,6 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Mono, muy buñuelo pues !!!");
+Console.WriteLine("buñuelo ud apá!!!");
+Console.WriteLine("buñuelo ud apá!!!");
+
 
 public class Personas {
 
